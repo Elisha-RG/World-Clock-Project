ecc9a4f39fee5428f52bf0beeb8be9c24f97b36f
Elisha-RG <egeoffr190@gmail.com>
2026-08-10 20:39:58 +0200

    completed js to ensure functionality,  adjusted style elements

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 function updateTime() {
   let munichComponent = document.querySelector("#munich");
-  let munichDateComponent = munichComponent.querySelector(".date");
-  let munichTimeComponent = munichComponent.querySelector(".time");
-  munichTime = moment().tz("Europe/Berlin");
+  if (munichComponent) {
+    let munichDateComponent = munichComponent.querySelector(".date");
+    let munichTimeComponent = munichComponent.querySelector(".time");
+    munichTime = moment().tz("Europe/Berlin");
 
-  munichDateComponent.innerHTML = munichTime.format("MMMM Do YYYY");
-  munichTimeComponent.innerHTML = munichTime.format(
-    "h:mm:ss [<small>]A[</small>]",
-  );
-
+    munichDateComponent.innerHTML = munichTime.format("MMMM Do YYYY");
+    munichTimeComponent.innerHTML = munichTime.format(
+      "h:mm:ss [<small>]A[</small>]",
+    );
+  }
   let seattleComponent = document.querySelector("#seattle");
-  let seattleDateComponent = seattleComponent.querySelector(".date");
-  let seattleTimeComponent = seattleComponent.querySelector(".time");
-  seattleTime = moment().tz("US/Pacific");
+  if (seattleComponent) {
+    let seattleDateComponent = seattleComponent.querySelector(".date");
+    let seattleTimeComponent = seattleComponent.querySelector(".time");
+    seattleTime = moment().tz("US/Pacific");
 
-  seattleDateComponent.innerHTML = seattleTime.format("MMMM Do YYYY");
-  seattleTimeComponent.innerHTML = seattleTime.format(
-    "h:mm:ss [<small>]A[</small>]",
-  );
+    seattleDateComponent.innerHTML = seattleTime.format("MMMM Do YYYY");
+    seattleTimeComponent.innerHTML = seattleTime.format(
+      "h:mm:ss [<small>]A[</small>]",
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesComponent = document.querySelector("#cities");
+  citiesComponent.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+        </div>
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectOption = document.querySelector("#city");
+
+citiesSelectOption.addEventListener("change", updateCity);
